3c373effd82982b83a999c9140e6ce18a3a0a625
z50205 <z50205@yahoo.com.tw>
2025-04-30 15:10:01 +0800

    Add email.js to send infos to backend, add email router.

--- static/scripts/email.js
+++ static/scripts/email.js
@@ -1,18 +1,28 @@
-let emailForm=document.getElementById("email");
 let BACKEND_IP="https://bizara.link";
-emailForm.addEventListener("submit",async (event)=>{
+const linkBtn=document.getElementsByClassName("attach-button")[0];
+const emailBtn = document.getElementsByClassName("submit-btn")[1];
+let shotImg=document.getElementById("screenShot")
+
+// 只要點擊連結的link就會觸發截圖功能，並將截圖存在shotImg裡面
+linkBtn.addEventListener("click",()=>{
+    html2canvas(document.body).then(canvas => {
+        shotImg.src = canvas.toDataURL("image/png");
+    });
+})
+// 點擊Email發送按鈕就會傳資料予後端
+emailBtn.addEventListener("click",async (event)=>{
     event.preventDefault();
-    const form=new FormData(event.target);
-    const email = document.getElementById("address").value;
-    const img = document.getElementById("screenShotImg");
-    const res = await fetch(img.src);
+    const emailInput = document.getElementsByClassName("email-input")[0];
+    
+    const email = emailInput.value;
+    const res = await fetch(shotImg.src);
     const blob = await res.blob();
 
     const formData = new FormData();
     formData.append("image", blob, "screenshot.png");
     formData.append("address", email);
-
-
+    formData.append("depature", state.currentSpot);
+    formData.append("destination", state.targetSpot);
     let response=await fetch(BACKEND_IP+"/api/email",{
         method: "POST",
         body:formData
@@ -20,15 +30,3 @@ emailForm.addEventListener("submit",async (event)=>{
     const result = await response.json();
     console.log(result);
 })
-
-let screenShotButton=document.getElementById("screenShot");
-let screenShotImg=document.getElementById("screenShotImg");
-
-screenShotButton.addEventListener("click",()=>{
-    html2canvas(document.querySelector("#capture")).then(canvas => {
-        screenShotImg.src = canvas.toDataURL("image/png");
-    });
-})
-
-const dataURL = canvas.toDataURL();
-console.log(dataURL);
